695539c5bd57157088015eed5e15bbafcfb8acfc
Yew Xuan <yewxuan_teo@mymail.sutd.edu.sg>
2020-12-01 03:05:30 +0800

    Added the swipable to show delete and edit qty Added new intent to receive the qty item Issue to fix: set firebase with updated qty

--- app/src/main/java/com/example/maptixdraft2/Homepage.java
+++ app/src/main/java/com/example/maptixdraft2/Homepage.java
@@ -1,14 +1,16 @@
 package com.example.maptixdraft2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,10 +19,13 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
+
 public class Homepage extends AppCompatActivity  {
     RecyclerView recyclerView;
     Button add_new_item;
     Button generate_map;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +39,6 @@ public class Homepage extends AppCompatActivity  {
             recyclerView = findViewById(R.id.grocery_list_recyclerview);
             recyclerView.hasFixedSize();
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-
-
-
 
 
 
@@ -62,33 +63,64 @@ public class Homepage extends AppCompatActivity  {
                     @Override
                     public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
 
-                        int position = viewHolder.getLayoutPosition(); //gets index of the viewholder being swiped
+                        final int position = viewHolder.getLayoutPosition(); //gets index of the viewholder being swiped
 
                         switch (direction) {
                             case ItemTouchHelper.LEFT:
-                                String deleteditem = null;
-                                deleteditem = myList.get(position).getItems();
+
+                                final String deleteditem = myList.get(position).getItems();
+                                final String deletedquantity = myList.get(position).getQuantity();
                                 Log.i("Test for item", deleteditem );
                                 myList.remove(viewHolder.getAdapterPosition()); //get position of the user list
                                 tableAdapter.notifyDataSetChanged();
 
                                 Log.i("Kewen delete","LayoutPosition is "+ position);
-                                Firebase.deleteItem(position, "Kewen"); //delete item based on index
-                                Toast.makeText(Homepage.this, "Item deleted" , Toast.LENGTH_SHORT).show();
+                                Firebase.deleteItem(deleteditem, "Kewen"); //delete item based on index
+                                Snackbar.make(recyclerView, deleteditem + " deleted",Snackbar.LENGTH_LONG).show();
+                                //to undo the delete (implement if time permits
+//                                Snackbar.make(recyclerView, deleteditem ,Snackbar.LENGTH_LONG).setAction("Undo", new View.OnClickListener() {
+//                                    @Override
+//                                    public void onClick(View v) {
+//                                        Firebase.addItem(deleteditem,deleteditem,"Kewen");
+//                                        Log.i("FB","Add deleted item to FB");
+//                                        tableAdapter.notifyItemInserted(position);
+//                                    }
+//                                }).show();
+
                                 break;
 
                                 case ItemTouchHelper.RIGHT:
+                                    String editeditem = myList.get(position).getItems();
+                                    Intent edit_intent = new Intent(Homepage.this, UpdateActivity.class);
+                                    edit_intent.putExtra("unlock_edit",editeditem);
+                                    startActivity(edit_intent);
                                     break;
 
                         }
                     }
+
+                    @Override
+                    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                        new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                                .addSwipeLeftBackgroundColor(ContextCompat.getColor(Homepage.this, R.color.red))
+                                .addSwipeLeftActionIcon(R.drawable.ic_baseline_delete_24)
+                                .addSwipeLeftLabel("Delete")
+                                .addSwipeRightBackgroundColor(ContextCompat.getColor(Homepage.this, R.color.turquoise))
+                                .addSwipeRightActionIcon(R.drawable.ic_baseline_edit_24)
+                                .addSwipeRightLabel("Edit Quantity")
+                                .create()
+                                .decorate();
+
+                        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+                    }
                 };
                 //to remove from the recycler view the item swiped
                 new ItemTouchHelper(item_delete).attachToRecyclerView(recyclerView);
-                //To do: delete item from firebase
+
 
             }
         };
+
         Firebase.displayShoppingList(displayShoppingListCallback,"Kewen"); //to trigger recyclerView to display user's shopping list retrieved from firebase
 
         //to add new item
---
+++ app/src/main/java/com/example/maptixdraft2/UpdateActivity.java
@@ -0,0 +1,73 @@
+package com.example.maptixdraft2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    TextView itemtobeupdated;
+    AutoCompleteTextView new_quantity;
+    Button update_button;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qtyedit);
+
+        Intent edit_intent = getIntent();
+        String item_to_be_edited = edit_intent.getStringExtra("unlock_edit");
+
+        itemtobeupdated = findViewById(R.id.update_Itemname);
+        new_quantity = findViewById(R.id.new_quantity);
+        update_button = findViewById(R.id.update_button);
+
+        itemtobeupdated.setText(item_to_be_edited);
+
+        final Firebase.booleanCallbackInterface addupdatedQuantityCallback = new Firebase.booleanCallbackInterface() {
+            @Override
+            public void onCallback(boolean itemExists) {
+                if (itemExists) {
+                    Intent edit_intent = getIntent();
+                    String item_to_be_edited = edit_intent.getStringExtra("unlock_edit");
+                    //Log.i("item", item_to_be_edited);
+                    Log.i("item",item_to_be_edited);
+                    String qtyEntered = new_quantity.getText().toString();
+                    Log.i("item_qty",qtyEntered);
+                    if (qtyEntered.equals("")) {
+                        qtyEntered = "-"; // if the user does not enter a quantity, we display a dash instead
+                    }
+                    ListItem newItemObject = new ListItem(item_to_be_edited, qtyEntered); //create new constructor
+                    Firebase.addItem(newItemObject,"Kewen"); //use this method instead of push() for correct firebase format and to avoid auto adding UUID
+                    Toast.makeText(UpdateActivity.this, "Item added successfully" , Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(UpdateActivity.this, "This item is not available at this supermarket!", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        update_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent edit_intent = getIntent();
+                String item_to_be_edited = edit_intent.getStringExtra("unlock_edit"); //Get the values from the autotextfield
+                Firebase.itemAvailability(addupdatedQuantityCallback,item_to_be_edited);
+
+            }
+        });
+
+
+
+
+
+    }
+}
